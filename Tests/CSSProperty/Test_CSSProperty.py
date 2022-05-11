f08fcae2d53738ab4ba9e58f6b2ac1f654cb368c
DirectiveAthena <directive.athena@gmail.com>
2022-05-11 15:07:54 +0200

    Feature: Early test structure of CSSproperty

---
+++ Tests/CSSProperty/Test_CSSProperty.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+
+# Custom Library
+
+# Custom Packages
+from BulkTests import BulkTests
+from AthenaCSS.Properties.CSSproperty import CSSproperty
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+
+class CSSProperty(BulkTests):
+
+    # noinspection PyUnreachableCode
+    def test_PropertySetup(self):
+        # Define a CSSProperty Class with a defined name
+        self.fail()
+
+        # Define a default value
+        self.fail()
+
+        # Define the Value range
+        self.fail()
+
+        # Define a value
+        self.fail()
+
+        # Print the value
+        self.fail()

1753f3e5d767fc79f86e3ae04f35af4aee15e94d
DirectiveAthena <directive.athena@gmail.com>
2022-05-12 16:21:15 +0200

    Feature: cursor

--- src/AthenaCSS/Library/PropertyLibrary.py
+++ src/AthenaCSS/Library/PropertyLibrary.py
@@ -1275,3 +1275,22 @@ class counter_reset(CSSproperty):
     )
     def __init__(self, value=value_logic.default, **kwargs):
         super().__init__(value, **kwargs)
+# ----------------------------------------------------------------------------------------------------------------------
+class cursor(CSSproperty):
+    name="cursor"
+    value_logic = ValueLogic(
+        default="auto",
+        value_choice={
+            None:None,
+            Url:Any,
+            str:{
+                "alias","all-scroll","auto","cell","context-menu","col-resize","copy","crosshair","default",
+                "e-resize","ew-resize","grab","grabbing","help","move","n-resize","ne-resize","nesw-resize",
+                "ns-resize","nw-resize","nwse-resize","no-drop","none","not-allowed","pointer","progress",
+                "row-resize","s-resize","se-resize","sw-resize","text","vertical-text","w-resize","wait",
+                "zoom-in","zoom-out","initial","inherit"
+            }
+        },
+    )
+    def __init__(self, value=value_logic.default, **kwargs):
+        super().__init__(value, **kwargs)

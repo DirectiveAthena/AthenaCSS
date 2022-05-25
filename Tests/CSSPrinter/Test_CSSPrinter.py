b0062f5e15277f060bdeb4a9275d23d164478b74
DirectiveAthena <directive.athena@gmail.com>
2022-05-25 14:53:37 +0200

    Feature: General Setup for the Printer class

---
+++ Tests/CSSPrinter/Test_CSSPrinter.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+
+# Custom Library
+import AthenaCSS.Library.SelectorElementLibrary as ElementLib
+from AthenaCSS.Objects.ElementSelection.CSSAttribute import CSSAttribute
+from AthenaCSS.Objects.ElementSelection.CSSClass import CSSClass
+from AthenaCSS.Objects.ElementSelection.CSSId import CSSId
+from AthenaCSS.Objects.ElementSelection.CSSSelection import CSSSelection
+
+# Custom Packages
+from BulkTests import BulkTests
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+class CSSPrinter(BulkTests):
+    def test_CSSPrinter(self):
+        pass

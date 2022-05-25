4429633752829a3818042a27a6d4a05e82dbbb38
DirectiveAthena <directive.athena@gmail.com>
2022-05-25 18:52:14 +0200

    Feature: Primary Init

---
+++ src/AthenaCSS/__init__.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+from AthenaCSS.Objects.Printer.CSSPrinter import CSSPrinter
+from AthenaCSS.Objects.Printer.PrinterColors import PrinterColors
+
+import AthenaCSS.Library.PropertyLibrary
+import AthenaCSS.Library.SubPropertyLibrary
+import AthenaCSS.Library.SelectorElementLibrary
+
+from AthenaCSS.Objects.ElementSelection.CSSAttribute import CSSAttribute
+from AthenaCSS.Objects.ElementSelection.CSSClass import CSSClass
+from AthenaCSS.Objects.ElementSelection.CSSElement import CSSElement
+from AthenaCSS.Objects.ElementSelection.CSSId import CSSId
+from AthenaCSS.Objects.ElementSelection.CSSPseudo import CSSPseudo
+from AthenaCSS.Objects.ElementSelection.CSSSelection import CSSSelection
+
+from AthenaCSS.Objects.Properties.CSSProperty import CSSProperty
+from AthenaCSS.Objects.Properties.CSSPropertyShorthand import CSSPropertyShorthand
+

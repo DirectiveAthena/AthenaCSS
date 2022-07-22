f09aca4da86a3c7fff21c057c776b0a6d4c1ec03
Andreas Sas <directive.athena@gmail.com>
2022-07-22 10:00:48 +0200

    Update Test_CSSGenerator.py

--- Tests/CSSGenerator/Test_CSSGenerator.py
+++ Tests/CSSGenerator/Test_CSSGenerator.py
@@ -17,61 +17,14 @@ from BulkTests import BulkTests
 
 class CSSGenerators(BulkTests):
     def test_CSSGenerator0(self):
-        with (css_generator := CSSGenerator()) as generator:
-            with (rule0 := CSSRule()) as (selector, declaration):
-                selector.add(
-                    SelectorElement.H1(CSSClass("something")),
-                    SelectorElement.H2(CSSClass("something_else")),
-                    SelectorElement.H2(CSSClass("something_else"), CSSClass("Something_totaly"), CSSClass("different")),
-                )
-                declaration.add(
-                    Property.Color(RGB(128,64,32)),
-                )
-            generator.add_rule(
-                rule0
-            ).add_comment(
-                "THIS SHOULD BE BETWEEN THE FIRST AND SECOND RULE"
-            )
-
-            with (rule1 := CSSRule()) as (selector, declaration):
-                selector.add(
-                    SelectorElement.H1
-                )
-                declaration.add(
-                    Property.Color(RGB(128,64,32)),
-                )
-
-            generator.add_rule(rule1)
-
-            with (rule2 := CSSRule()) as (selector, declaration):
-                id_ = CSSId("Help", "Me")
-                selector.add(
-                    SelectorElement.H1,
-                    SelectorElement.H2(id_)
-                )
-                declaration.add(
-                    Property.Color(RGB(128,64,32)),
-                    Property.BackgroundColor(RGB(128,64,32)),
-                    Property.BorderColor()
-                )
-
-            generator.add_rule(rule2)
+        css_generator = CSSGenerator().add_rule(
+            selectors=[CSSElement.H1.classes("something")],
+            declarations = {"color": RGB(128, 64, 32)}
+        )
 
         self.assertEqual(
-"""h1.something,
-h2.something_else,
-h2.something_else.Something_totaly.different {
+"""h1.something {
     color: rgb(128, 64, 32);
-}
-/*THIS SHOULD BE BETWEEN THE FIRST AND SECOND RULE*/
-h1 {
-    color: rgb(128, 64, 32);
-}
-h1,
-h2#Help#Me {
-    color: rgb(128, 64, 32);
-    background-color: rgb(128, 64, 32);
-    border-color: transparent;
 }""",
         css_generator.to_string()
         )

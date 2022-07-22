27e3507ff7391309e2e6d7433981fcdd9283f29d
Andreas Sas <directive.athena@gmail.com>
2022-07-22 09:42:41 +0200

    Cleanup

--- src/AthenaCSS/functions/__init__.py
+++ src/AthenaCSS/functions/__init__.py
@@ -2,16 +2,11 @@
 # - Package Imports -
 # ----------------------------------------------------------------------------------------------------------------------
 # General Packages
-from __future__ import annotations
 
 # Custom Library
 
 # Custom Packages
-from AthenaCSS.models.declarations.property import CSSProperty
 
 # ----------------------------------------------------------------------------------------------------------------------
-# - SubProperty -
+# - Code -
 # ----------------------------------------------------------------------------------------------------------------------
-class CSSSubProp(CSSProperty):
-    def printer(self) -> str:
-        return f"{self.name}({self._value.printer()})"
